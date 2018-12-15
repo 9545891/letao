4343643924705b6a85f71c226285be25bd351bfa
wenbo9545891@163.com <wenbo9545891@163.com>
2018-12-15 16:46:09 +0800

    登陆功能完成

---
+++ public/back/js/login.js
@@ -0,0 +1,98 @@
+$(function() {
+  // 1.验证表单
+
+  //使用表单校验插件
+  $('#form').bootstrapValidator({
+    //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
+    // excluded: [':disabled', ':hidden', ':not(:visible)'],
+
+    //2. 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+
+    //3. 指定校验字段
+    fields: {
+      //校验用户名，对应name表单的name属性
+      username: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '用户名不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 2,
+            max: 16,
+            message: '用户名长度必须在2到16之间'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^[a-zA-Z0-9_\.]+$/,
+            message: '用户名由数字字母下划线和.组成'
+          },
+          callback: {
+            message: '用户名不存在'
+          }
+        }
+      },
+      password: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '密码不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 2,
+            max: 16,
+            message: '密码长度必须在2到16之间'
+          },
+          callback: {
+            message: '密码错误'
+          }
+        }
+      }
+    }
+  })
+
+  // 2.阻止默认提交行为
+  // 通过ajax提交
+
+  $('#form').on('success.form.bv', function(e) {
+    e.preventDefault()
+    $.ajax({
+      type: 'post',
+      url: '/employee/employeeLogin',
+      data: $('#form').serialize(),
+      dataType: 'json',
+      success: function(res) {
+        if (res.success) {
+          location.href = 'index.html'
+        }
+        if (res.error === 1000) {
+          //   alert('用户名不存在')
+          $('#form')
+            .data('bootstrapValidator')
+            .updateStatus('username', 'INVALID', 'callback')
+        }
+        if (res.error === 1001) {
+          //   alert('密码错误')
+          $('#form')
+            .data('bootstrapValidator')
+            .updateStatus('password', 'INVALID', 'callback')
+        }
+      }
+    })
+  })
+
+  // 3.表单重置功能
+
+  $('[type="reset"]').click(function() {
+    $('#form')
+      .data('bootstrapValidator')
+      .resetForm()
+  })
+})
